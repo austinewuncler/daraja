41069ed169bd50ddd718e6e3ab010811afdac648
Austine Were <austinewuncler@gmail.com>
2021-02-16 23:02:54 +0300

    refactor(http): :recycle: replace axios with got
    got is easier to mock using nock library

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
-import axios from "axios";
+import got from "got";
 
 export default class Daraja {
   static BASE_URL = "https://{}.safaricom.co.ke";
-  static GENERATE_ACCESS_TOKEN = "/oauth/v1/generate";
-  static MPESA_EXPRESS_REQUEST = "/mpesa/stkpush/v1/processrequest";
+  static GENERATE_ACCESS_TOKEN = "oauth/v1/generate";
+  static MPESA_EXPRESS_REQUEST = "mpesa/stkpush/v1/processrequest";
   private accessToken: string;
   private tokenExpiry: number;
   private baseURL: string;
-  private consumerKey: string;
-  private consumerSecret: string;
   private lnmPasskey?: string;
 
   constructor(
     private shortcode: number,
+    private consumerKey: string,
+    private consumerSecret: string,
     config: {
       env?: "sandbox" | "production";
-      consumerKey: string;
-      consumerSecret: string;
-      lnmPasskey: string;
+      lnmPasskey?: string;
     }
   ) {
     this.accessToken = "";
@@ -26,8 +24,7 @@ export default class Daraja {
       "{}",
       config.env === "production" ? "api" : "sandbox"
     );
-    this.consumerKey = config.consumerKey;
-    this.consumerSecret = config.consumerSecret;
+    this.lnmPasskey = config.lnmPasskey;
   }
 
   private static generateTimestamp = (date: Date) =>
@@ -38,71 +35,69 @@ export default class Daraja {
     `${date.getMinutes()}`.padStart(2, "0") +
     `${date.getSeconds()}`.padStart(2, "0");
 
-  private refreshToken = async () => {
+  private refreshToken = async (): Promise<void> => {
     if (Date.now() >= this.tokenExpiry) {
       try {
-        const {
-          data: { access_token, expires_in }
-        } = await axios.get(Daraja.GENERATE_ACCESS_TOKEN, {
-          baseURL: this.baseURL,
-          params: { grant_type: "client_credentials" },
-          auth: {
+        const { access_token, expires_in } = await got
+          .get(Daraja.GENERATE_ACCESS_TOKEN, {
+            prefixUrl: this.baseURL,
+            searchParams: new URLSearchParams({
+              grant_type: "client_credentials"
+            }),
             username: this.consumerKey,
             password: this.consumerSecret
-          }
-        });
-        console.log(this.consumerKey + " " + this.consumerSecret);
+          })
+          .json();
         this.accessToken = access_token;
         this.tokenExpiry = Date.now() + +expires_in * 1000;
       } catch (err) {
-        throw new Error(err.response.statusText);
+        throw new Error(err.response.statusMessage);
       }
     }
   };
 
-  mpesaExpressRequest = async (options: {
-    amount: number;
-    senderMSISDN: number;
-    recipientShortcode?: number;
-    stkPushMSISDN?: number;
-    transactionType: "CustomerBuyGoodsOnline" | "CustomerPayBillOnline";
-    callbackURL: string;
-    accountReference: string;
-    transactionDesc: string;
-  }): Promise<{ merchantRequestID: string; checkoutRequestID: string }> => {
+  mpesaExpressRequest = async (
+    amount: number,
+    partyA: number,
+    transactionType: "CustomerBuyGoodsOnline" | "CustomerPayBillOnline",
+    options: {
+      partyB?: number;
+      phoneNumber?: number;
+      callbackURL: string;
+      accountReference: string;
+      transactionDesc: string;
+    }
+  ): Promise<{ merchantRequestID: string; checkoutRequestID: string }> => {
     await this.refreshToken();
     const currentTimestamp = Daraja.generateTimestamp(new Date());
     try {
       const {
-        data: {
-          MerchantRequestID: merchantRequestID,
-          CheckoutRequestID: checkoutRequestID
-        }
-      } = await axios.post(
-        Daraja.MPESA_EXPRESS_REQUEST,
-        {
-          BusinessShortCode: this.shortcode,
-          Password: Buffer.from(
-            `${this.shortcode}${this.lnmPasskey}${currentTimestamp}`
-          ).toString("base64"),
-          Timestamp: currentTimestamp,
-          TransactionType: options.transactionType,
-          Amount: options.amount,
-          PartyA: options.senderMSISDN,
-          PartyB: options.recipientShortcode || this.shortcode,
-          PhoneNumber: options.stkPushMSISDN || options.senderMSISDN,
-          CallBackURL: options.callbackURL,
-          AccountReference: options.accountReference,
-          TransactionDesc: options.transactionDesc
-        },
-        {
-          baseURL: this.baseURL,
+        MerchantRequestID: merchantRequestID,
+        CheckoutRequestID: checkoutRequestID
+      } = await got
+        .post(Daraja.MPESA_EXPRESS_REQUEST, {
+          prefixUrl: this.baseURL,
+          json: {
+            BusinessShortCode: this.shortcode,
+            Password: Buffer.from(
+              `${this.shortcode}${this.lnmPasskey}${currentTimestamp}`
+            ).toString("base64"),
+            Timestamp: currentTimestamp,
+            TransactionType: transactionType,
+            Amount: amount,
+            PartyA: partyA,
+            PartyB: options.partyB || this.shortcode,
+            PhoneNumber: options.phoneNumber || partyA,
+            CallBackURL: options.callbackURL,
+            AccountReference: options.accountReference,
+            TransactionDesc: options.transactionDesc
+          },
           headers: { Authorization: `Bearer ${this.accessToken}` }
-        }
-      );
+        })
+        .json();
       return { merchantRequestID, checkoutRequestID };
     } catch (err) {
-      throw new Error(err.response.data.errorMessage);
+      throw new Error(JSON.parse(err.response.body).errorMessage);
     }
   };
 }
